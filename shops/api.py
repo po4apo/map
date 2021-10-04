acb9315c12cb24eba8667b5c6401215c650aeecc
main <suhar20001@mail.ru>
2021-10-04 21:37:21 +0400

    Final version.

--- shops/api.py
+++ shops/api.py
@@ -4,28 +4,22 @@ from rest_framework.decorators import api_view
 from shops.models import Shop, Street, City
 from shops.serializers import ShopSerializer, StreetSerializer, CitySerializer, ShopSerializerDetal
 
-@api_view(['GET', 'POST'])
+
+@api_view(['GET'])
 def city_list(request):
     """
-    List all cities, or create a new city.
+    List all cities.
     """
     if request.method == 'GET':
         cities = City.objects.all()
         serializer = CitySerializer(cities, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = CitySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 @api_view(['GET'])
 def all_streets_in_the_current_city(request, city_id):
     """
-    Retrieve, update or delete a code snippet.
+    List all streets in the current city.
     """
     try:
         streets = Street.objects.filter(city_id=city_id)
@@ -36,7 +30,8 @@ def all_streets_in_the_current_city(request, city_id):
         serializer = StreetSerializer(streets, many=True)
         return Response(serializer.data)
 
-@api_view(['GET','POST'])
+
+@api_view(['GET', 'POST'])
 def add_shop(request):
     """
     List shops filtered on params.
@@ -54,18 +49,18 @@ def add_shop(request):
     }
     """
     try:
-        #?street=&city=&open=0/1
-        #get_params
+        # ?street=&city=&open=0/1
+        # get_params
         params = {'street_id': request.GET.get('street', None),
                   'city_id': request.GET.get('city', None),
                   }
         user_param_for_field_open = request.GET.get('open')
         # normalize_data
-        valid_params = {k: v for k, v in params.items() if (v.isdigit())}
+        valid_params = {k: v for k, v in params.items() if (v is not None and v.isdigit())}
         # get_data_from_db
         shops = Shop.objects.filter(**valid_params)
-        if user_param_for_field_open.isdigit():
-            shops = [x for x in shops if (x.open== int(user_param_for_field_open))]
+        if user_param_for_field_open is not None and user_param_for_field_open.isdigit():
+            shops = [x for x in shops if (x.open == int(user_param_for_field_open))]
 
     except Shop.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
@@ -80,6 +75,3 @@ def add_shop(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-
-
-
